f9920a3836fa99b514a627aa75956c2720290617
luka <luka@hakiri.xyz>
2026-04-09 19:30:00 +0000

    feat(decode): __init__ re-exports

--- src/hakiri/decode/__init__.py
+++ src/hakiri/decode/__init__.py
@@ -1,1 +1,14 @@
-"""log decoders."""
+"""log decoders. converts rpc receipts into hakiri.core.types.SwapTx."""
+
+from hakiri.decode.routers import KNOWN_ROUTERS, is_known_router
+from hakiri.decode.uniswap_v2 import V2_SWAP_TOPIC, decode_v2_swap
+from hakiri.decode.uniswap_v3 import V3_SWAP_TOPIC, decode_v3_swap
+
+__all__ = [
+    "KNOWN_ROUTERS",
+    "V2_SWAP_TOPIC",
+    "V3_SWAP_TOPIC",
+    "decode_v2_swap",
+    "decode_v3_swap",
+    "is_known_router",
+]
