c0a4fceb78ebbcb9e90f362fbefb000aaa76bac6
luka <luka@hakiri.xyz>
2026-04-09 14:30:00 +0000

    feat(decode): cowswap + 0x labels

--- src/hakiri/decode/routers.py
+++ src/hakiri/decode/routers.py
@@ -11,6 +11,9 @@ KNOWN_ROUTERS: Dict[str, str] = {
     "0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45": "uniswap-v3-router-2",
     "0x3fC91A3afd70395Cd496C647d5a6CC9D4B2b7FAD": "universal-router",
     "0x1111111254EEB25477B68fb85Ed929f73A960582": "1inch-v5",
+    "0x111111125421cA6dc452d289314280a0f8842A65": "1inch-v6",
+    "0x9008D19f58AAbD9eD0D60971565AA8510560ab41": "cowswap-vault",
+    "0xDef1C0ded9bec7F1a1670819833240f027b25EfF": "0x-exchange-proxy",
 }
 
 
