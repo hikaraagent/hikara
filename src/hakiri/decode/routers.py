830adeab5d298ec01041810588ac1a0c2f79a198
luka <luka@hakiri.xyz>
2026-04-09 10:15:00 +0000

    feat(decode): more router labels

--- src/hakiri/decode/routers.py
+++ src/hakiri/decode/routers.py
@@ -9,6 +9,8 @@ KNOWN_ROUTERS: Dict[str, str] = {
     "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D": "uniswap-v2-router",
     "0xE592427A0AEce92De3Edee1F18E0157C05861564": "uniswap-v3-router",
     "0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45": "uniswap-v3-router-2",
+    "0x3fC91A3afd70395Cd496C647d5a6CC9D4B2b7FAD": "universal-router",
+    "0x1111111254EEB25477B68fb85Ed929f73A960582": "1inch-v5",
 }
 
 
