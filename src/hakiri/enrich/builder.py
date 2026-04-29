8f2257fdd607095963012f5a33092347233c5589
boss <mac@worker2013.local>
2026-04-29 16:06:53 +0300

    style(enrich): ruff import order

--- src/hakiri/enrich/builder.py
+++ src/hakiri/enrich/builder.py
@@ -4,7 +4,6 @@ from __future__ import annotations
 
 from typing import Dict, Optional
 
-
 KNOWN_BUILDERS: Dict[str, str] = {
     "0x690B9A9E9aa1C9dB991C7721a92d351Db4FaC990": "builder0x69",
     "0xE8DDAd86796E7416c0E62D1Bf90e54a4C2Dabd28": "rsync-builder",
--- src/hakiri/enrich/searcher.py
+++ src/hakiri/enrich/searcher.py
@@ -4,7 +4,6 @@ from __future__ import annotations
 
 from typing import Dict
 
-
 KNOWN_SEARCHERS: Dict[str, str] = {
     "0xa69BabEF1cA67A37Ffaf7a485DfFF3382056e78C": "jaredfromsubway-1",
     "0x00000000003b3cc22aF3aE1EAc0440BcEe416B40": "rsync-builder-bot",
